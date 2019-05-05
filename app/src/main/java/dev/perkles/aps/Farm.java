08069fd4fbfd40654b0165e331ca8ca976d91812
Perkles <otaviocha@gmail.com>
2019-05-05 15:52:15 -0300

    Iniciado a implementacao de #2
    Escopo basico do Drag and Drop implementado.

--- app/src/main/java/dev/perkles/aps/Farm.java
+++ app/src/main/java/dev/perkles/aps/Farm.java
@@ -1,7 +1,11 @@
 package dev.perkles.aps;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
 
 public class Farm extends AppCompatActivity {
 
@@ -9,5 +13,49 @@ public class Farm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm);
+
+        findViewById(R.id.imageView_sheep_placeholder).setOnLongClickListener(new MyOnLongClickListener());
+        findViewById(R.id.imageView_sheep).setOnLongClickListener(new MyOnLongClickListener());
+    }
+
+    class MyOnLongClickListener implements View.OnLongClickListener {
+
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("simple_test", "teste");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, shadowBuilder, v, 0);
+            v.setVisibility(View.INVISIBLE);
+            return true;
+        }
+    }
+
+    class MyOnDragListener implements View.OnDragListener {
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            int action = event.getAction();
+
+            switch (action){
+                case DragEvent.ACTION_DRAG_STARTED:
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    break;
+
+                case DragEvent.ACTION_DROP:
+                    break;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+
+                case DragEvent.ACTION_DRAG_ENDED:
+                    break;
+            }
+            return true;
+        }
     }
 }
